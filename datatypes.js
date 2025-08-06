5e89e16c0cab4226dd18128bf692ebf444b54959
SANDHYA2004-droid <sandhyaleela9@gmail.com>
2025-08-06 10:30:02 +0530

    Update and rename demo.js to datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let text = "Hello";   
+let number = 42;     
+let isOnline = true;  
+let fruits = ["apple", "mango"]; 
+let person = { name: "Sagar", age: 25 }; 
--- demo.js
+++
@@ -1,5 +0,0 @@
-let text = "Hello";   // String (text)
-let number = 42;      // Number
-let isOnline = true;  // Boolean (true/false)
-let fruits = ["apple", "mango"]; // Array
-let person = { name: "Sagar", age: 25 }; // Object
