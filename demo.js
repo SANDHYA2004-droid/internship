1831e0f7763cb55373469914f64cf31a93432382
SANDHYA2004-droid <sandhyaleela9@gmail.com>
2025-08-06 10:25:31 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,5 @@
+let text = "Hello";   // String (text)
+let number = 42;      // Number
+let isOnline = true;  // Boolean (true/false)
+let fruits = ["apple", "mango"]; // Array
+let person = { name: "Sagar", age: 25 }; // Object
