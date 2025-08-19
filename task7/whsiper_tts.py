dff4131424a0c61fd1e3c624ec08071bb7f62655
SANDHYA2004-droid <sandhyaleela9@gmail.com>
2025-08-19 22:03:51 +0530

    Delete task7 directory

--- task7/whsiper_tts.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-# Load once (will be reused from cache after first time)
-model = whisper.load_model("small")
-
-# Transcribe your file
-result = model.transcribe("ttsMP3.com_VoiceText_2025-8-19_19-46-39.mp3")
-
-print(result["text"])
